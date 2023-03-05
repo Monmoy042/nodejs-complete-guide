3a7fb826f2327e3d887f996356460e4278e816e1
Monmoy042 <monmoykms.16@gmail.com>
2023-03-05 21:39:04 +0600

    Template engine:pug

--- routes/admin.js
+++ routes/admin.js
@@ -6,17 +6,31 @@ const rootDir = require("../helpers/path");
 
 const router = express.Router();
 
+const products = [];
+
+// /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
   // console.log("In the another middleware");
   // res.send(
   //   "<form action='/admin/product' method='POST'><input type='text' name='title' ></input><button type='submit'>Add Product</button></form>"
   // );
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+
+  // Serving html files
+  // res.sendFile(path.join(rootDir, "views", "add-product.html"));
+
+  // Serving template engine file pug
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+  });
 });
 
+// /admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
-  console.log(req.body);
+  // console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -3,13 +3,21 @@ const path = require("path");
 const express = require("express");
 
 const rootDir = require("../helpers/path");
+const adminData = require("./admin");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
   // console.log("In the another middleware");
   // res.send("<h1>Hello from express</h1>");
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+
+  // Serving HTML file
+  // console.log(adminData.products);
+  // res.sendFile(path.join(rootDir, "views", "shop.html"));
+
+  // Serving Template Engine:pug
+  const products = adminData.products;
+  res.render("shop", { prods: products, pageTitle: "Shop", path: "/" });
 });
 
 module.exports = router;
